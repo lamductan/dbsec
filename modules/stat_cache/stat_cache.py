def1325ecf88efb2a060e3875257d7078a5a18d5
JonahFarc <jonahfarc@gmail.com>
2020-04-30 02:06:03 -0500

    Untested metadata changes.. works theoretically

--- modules/stat_cache/stat_cache.py
+++ modules/stat_cache/stat_cache.py
@@ -1,9 +1,7 @@
 import os
-import sys
 import shutil
 
-from utils.utils import make_dirs, cache_data, restore_data 
-
+from utils.utils import make_dirs, cache_data, restore_data, split_file
 
 class StatCache(object):
 	
@@ -15,21 +13,22 @@ class StatCache(object):
 		self._latest_stat_cache_dir = os.path.join(stat_cache_dir, "latest")
 		make_dirs(self._latest_stat_cache_dir)
 
-		#newest file backup; compared to latest
+		# newest file backup; compared to latest
 		self._new_stat_cache_dir = os.path.join(stat_cache_dir, "new")
 		make_dirs(self._new_stat_cache_dir)
 
-		#root stat file
+		# root stat file
 		self._root_stat = os.path.join(self._latest_stat_cache_dir,
-			os.path.basename(self._backup_folder) + ".dir")
+									   os.path.basename(self._backup_folder) + ".dir")
 
 		self.initialized = True
 
-		self.metadata = {
-			"previous": {},
-			"key": "",
-			"new": {}
-		}
+		# self.metadata = {
+		# 	"previous": {},
+		# 	"key": "",
+		# 	"new": {}
+		# }
+		self.metadata = {}
 
 	def getLocalPath(self, path):
 		# return str(path.path).replace(self._backup_folder, "")[1:]	#drop the leading slash
@@ -73,18 +72,22 @@ class StatCache(object):
 			print(restore_data(latestStatPath))
 			modified = True
 
-		#if this is a directory, recursively check
+		# if this is a directory, recursively check
 		if os.path.isdir(path):
 			for f in os.scandir(path):
 				file_updated = self.recursive_file_check(f)
 				if not modified and file_updated:
 					modified = True
+		elif os.path.isfile(path):
+			if modified:
+				self.update_metadata(path)
+
 		return modified
 
 	def is_backup_folder_modified(self):
 		modified = False
-		if not os.path.isfile(self._root_stat):	#first time executing
-			modified = True	#should this be modified? data might not be in cloud yet
+		if not os.path.isfile(self._root_stat):  # first time executing
+			modified = True  # should this be modified? data might not be in cloud yet
 			# self.initialized = False
 			self.recursive_file_check(self._backup_folder)
 		else:
@@ -92,11 +95,55 @@ class StatCache(object):
 			modified = self.recursive_file_check(self._backup_folder)
 		return modified
 
+	def update_metadata(self, path):
+		# Set or update version number
+		metadata = self.metadata[os.path.basename(path)]
+		if not metadata:
+			metadata["version"] = 1
+		else:
+			metadata["version"] += 1
+
+		curr_metadata = metadata[metadata["version"]]
+		prev_metadata = metadata[metadata["version"] - 1]
+
+		out_dir = os.path.join(os.path.join(os.path.dirname(path), os.path.basename(path)),
+							   self.metadata[path]["version"])
+		split_file(path, out_dir)
+		index = 0
+		for file in os.listdir(out_dir):
+			prev = False
+			if metadata["version"] > 1:
+				if index in curr_metadata["previous"]:
+					prev_out_dir = os.path.join(os.path.join(os.path.dirname(path), os.path.basename(path)),
+												prev_metadata["previous"][index])
+				else:
+					prev_out_dir = os.path.join(os.path.join(os.path.dirname(path), os.path.basename(path)),
+												self.metadata[path]["version"] - 1)
+
+				if self.chunks_equal(prev_out_dir, out_dir, file):
+					prev = True
+			if prev:
+				if index in prev_metadata["previous"]:
+					curr_metadata["previous"][index] = prev_metadata["previous"][index]
+				else:
+					curr_metadata["previous"][index] = metadata["version"] - 1
+			else:
+				curr_metadata["new"] += index
+
+		return 0
+
+	def chunks_equal(self, prev_out_dir, out_dir, file, chunk_size=1000000):
+		with open(os.path.join(prev_out_dir, file), "rb") as f1:
+			with open(os.path.join(out_dir, file), "rb") as f2:
+				prev_chunk = f1.read(chunk_size)
+				curr_chunk = f2.read(chunk_size)
+		return prev_chunk == curr_chunk
+
 	def set_metadata(self, data):
 		# TODO: load the metadata pulled from blockchain into here
 		pass
 
 	def update_new_cache(self):
-		if self.initialized:	#we only want to update the latest with the new cache if new cache is present
+		if self.initialized:  # we only want to update the latest with the new cache if new cache is present
 			shutil.rmtree(self._latest_stat_cache_dir)
 			shutil.move(self._new_stat_cache_dir, self._latest_stat_cache_dir)
