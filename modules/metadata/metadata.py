146bb057c1d2b86f1f91b84b6b0cd3f621ac6bbb
JonahFarc <jonahfarc@gmail.com>
2020-04-30 15:54:16 -0500

    Fixing of file naming

--- modules/metadata/metadata.py
+++ modules/metadata/metadata.py
@@ -8,28 +8,48 @@ class Metadata(object):
 
     def update_metadata(self, path):
         # Set or update version number
-        metadata = self.metadata[os.path.basename(path)]
+        basename = str(os.path.basename(path))
+        if basename not in self.metadata:
+            self.metadata[basename] = {}
+        metadata = self.metadata[basename]
         if not metadata:
             metadata["version"] = 1
+            metadata[1] = {}
         else:
             metadata["version"] += 1
+            metadata[metadata["version"]] = {}
+            prev_metadata = metadata[metadata["version"] - 1]
 
         curr_metadata = metadata[metadata["version"]]
-        prev_metadata = metadata[metadata["version"] - 1]
+        curr_metadata["new"] = []
+        curr_metadata["previous"] = []
 
-        out_dir = os.path.join(os.path.join(os.path.dirname(path), os.path.basename(path)),
-                               self.metadata[path]["version"])
-        split_file(path, out_dir)
+        print(os.path.join(os.path.join(os.path.dirname(path), basename + "_chunks"),
+                           ("v" + str(self.metadata[basename]["version"]))))
+
+        if not os.path.exists(os.path.join(os.path.dirname(path), basename + "_chunks")):
+            os.mkdir(os.path.join(os.path.dirname(path), basename + "_chunks"), )
+
+        if not os.path.exists(os.path.join(os.path.join(os.path.dirname(path), basename + "_chunks"),
+                                           ("v" + str(self.metadata[basename]["version"])))):
+            os.mkdir(os.path.join(os.path.join(os.path.dirname(path), basename + "_chunks"),
+                                  ("v" + str(self.metadata[basename]["version"]))))
+
+        out_dir = str(os.path.join(os.path.join(os.path.dirname(path), basename + "_chunks"),
+                                   str(self.metadata[basename]["version"])))
+        print(os.path.join(os.path.dirname(os.path.abspath(__file__)), path))
+        print(os.path.isfile(os.path.join(os.path.abspath(os.getcwd()), path)))
+        split_file(os.path.join(os.path.abspath(os.getcwd()), path), out_dir)
         index = 0
         for file in os.listdir(out_dir):
             prev = False
             if metadata["version"] > 1:
                 if index in curr_metadata["previous"]:
-                    prev_out_dir = os.path.join(os.path.join(os.path.dirname(path), os.path.basename(path)),
-                                                prev_metadata["previous"][index])
+                    prev_out_dir = str(os.path.join(os.path.join(os.path.dirname(path), basename + "chunks"),
+                                                    prev_metadata["previous"][index]))
                 else:
-                    prev_out_dir = os.path.join(os.path.join(os.path.dirname(path), os.path.basename(path)),
-                                                self.metadata[path]["version"] - 1)
+                    prev_out_dir = str(os.path.join(os.path.join(os.path.dirname(path), basename + "chunks"),
+                                                    self.metadata[basename]["version"] - 1))
 
                 if self.chunks_equal(prev_out_dir, out_dir, file):
                     prev = True
@@ -39,7 +59,7 @@ class Metadata(object):
                 else:
                     curr_metadata["previous"][index] = metadata["version"] - 1
             else:
-                curr_metadata["new"] += index
+                curr_metadata["new"] += [index]
 
         return 0
 
